41fbe1fed38b62f121f0482393bd70a25c628c9f
Natthawut.K <foliose.k.natthawut@gmail.com>
2020-08-10 17:43:59 +0700

    add aura and play animation

--- IAmRich/ViewController.swift
+++ IAmRich/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var theDiamon: UIImageView!
+    @IBOutlet weak var aura: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UIView.animate(withDuration: 3, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.theDiamon.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.aura.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        })
     }
-
-
+    
+    
 }
 
